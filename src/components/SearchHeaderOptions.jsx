e7ae0b0cfa8c93db890e1e726757ffe500265597
damijan0034 <damijan0034@gmail.com>
2023-03-17 05:52:40 -0700

    add search header options

--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -1,7 +1,30 @@
+"use client"
+
 import React from 'react'
+import {AiOutlineSearch} from 'react-icons/ai';
+import { usePathname,useRouter,useSearchParams } from 'next/navigation';
 
 export default function SearchHeaderOptions() {
+
+  const pathName=usePathname();
+  const router=useRouter();
+  const searchParams=useSearchParams();
+  const searchTerm=searchParams.get('searchTerm');
+
+  function selectTab(tab){
+    router.push(`/search/${tab === "Images" ? "image" : "web"}?searchTerm=${searchTerm}`)
+  }
   return (
-    <div>SearchHeaderOptions</div>
+    <div className='flex justify-center sm:justify-start space-x-2 border-b text-gray-700 text-sm pl-3'>
+      <div onClick={()=>selectTab("All")} className={`${pathName === "/search/web" && "text-blue-600 border-b-blue-600"} flex items-center space-x-1 border-b-4 border-transparent pb-3 px-2 cursor-pointer active:text-blue-500`}>
+      <AiOutlineSearch className='text-md' />
+      <p>All</p>
+    </div>
+    <div onClick={()=>selectTab("Images")} className={`${pathName === "/search/image" && "text-blue-600 border-b-blue-600"} flex items-center space-x-1 border-b-4 border-transparent pb-3 px-2 cursor-pointer active:text-blue-500`}>
+      <AiOutlineSearch className='text-md' />
+      <p>Images</p>
+    </div>
+    </div>
+    
   )
 }

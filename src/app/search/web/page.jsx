8b2926874b9d26564894dd10549c70cd7f2fb6f5
damijan0034 <damijan0034@gmail.com>
2023-03-18 01:26:56 -0700

    add google seo

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default async function page() {
-  const response=await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=lectures`);
+export default async function page({searchParams}) {
+  const response=await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
   const data= await response.json();
   console.log(data);
   const results=data.items;

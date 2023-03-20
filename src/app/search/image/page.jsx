804c17f5c555befb86e942c13a65c10ef13110c3
damijan0034 <damijan0034@gmail.com>
2023-03-20 02:26:46 -0700

    add image results

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,7 +1,29 @@
+import ImageSearchResults from '@/components/ImageSearchResults';
+
+import Link from 'next/link';
 import React from 'react'
 
-export default function page() {
+export default async function imageSearch({searchParams}) {
+  const response=await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`);
+  if(!response.ok){
+    throw new Error("Something went wrong")
+  }
+  const data= await response.json();
+  console.log(data);
+  const results=data.items;
+  if(!results){
+    return (
+    <div className='flex pt-10 flex-col justify-center items-center'>
+        <h1 className='text-4xl'>No results found</h1>
+        <p>Try something else</p>
+        <Link className='text-blue-500' href='/'>Home </Link>
+    </div>
+    )
+    
+  }
   return (
-    <div>page</div>
+    <>
+      {results && <ImageSearchResults results={data} />}
+    </>
   )
 }
---
+++ src/components/ImageSearchResults.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function ImageSearchResults({results}) {
+  return (
+    <div className='pb-40 mt-4'>
+        <div className=' px-3 space-x-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
+            
+            {results.items.map(result=>(
+                <div key={result.link}>
+                       <div className='group'>
+                            <Link href={result.image.contextLink}>
+                                <img className='h-60 hover:shadow-lg w-full object-contain transition-shadow' src={result.link} alt={result.title} />
+                            </Link>
+                            <Link href={result.image.contextLink}>
+                                <h2 className='hover:underline text-xl truncate'>{result.title}</h2>
+                            </Link>
+                            <Link href={result.image.contextLink}>
+                                <p className='hover:underline text-gray-600'>{result.displayLink}</p>
+                            </Link>
+                        </div> 
+                </div>
+                
+            ))}
+        </div>
+    </div>
+  )
+}
